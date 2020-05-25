26b362a31284d9e065c91c9775d21495901340bd
Roland Tamas <rolandtamas98@gmail.com>
2020-05-25 11:49:09 +0300

    Added the navbar and modfied some files

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,11 +5,13 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsersService } from '../users/users.service';
 import { UsersComponent } from '../users/users.component';
+import { NavbarComponent } from '../navbar/navbar.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsersComponent
+    UsersComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
