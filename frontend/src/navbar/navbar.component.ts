9fb1a857c0f5541bd0fc928210370886ee49ecc0
Roland Tamas <rolandtamas98@gmail.com>
2020-07-09 00:13:42 +0300

    It's good to be back. Changed the navbar

--- frontend/src/navbar/navbar.component.ts
+++ frontend/src/navbar/navbar.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
-  title = 'Ember';
+  title = 'My Project';
   constructor() { }
 
   ngOnInit(): void {
